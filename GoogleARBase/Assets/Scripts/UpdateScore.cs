3fef9416fdacba070efdf89ecd2c0c36f4c963f7
Nate Kelley <natemkelley@gmail.com>
2018-12-05 14:35:21 -0700

    score script update
    updated score script

---
+++ GoogleARBase/Assets/Scripts/UpdateScore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+//attach this to the hoop
+public class UpdateScore : MonoBehaviour {
+    private int score = 0;
+    public Text scoreText;
+
+    // Use this for initialization
+    private void Start()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerExit(Collider other)
+    {
+        score += 1;
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
